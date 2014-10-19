6c34d0a464abd549cccfd32a1d2729d16b28e5e8
Kevin Ngo <ngoke@mozilla.com>
2014-10-19 13:29:03 -0700

    add camera

--- mozfestApp/www/js/controllers.js
+++ mozfestApp/www/js/controllers.js
@@ -1,6 +1,22 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+    // Exposes click handler to the template via the $scope object.
+    $scope.takePicture = function() {
+        if (!navigator.camera) {
+            console.log('App is not yet built nor running on actual device');
+            return;
+        }
+
+        // Calls the camera API. Cordova will abstract this to work with all
+        // devices natively.
+        navigator.camera.getPicture(function(imageURI) {
+            // Exposes the image src to the template via the $scope object.
+            window.document.getElementById('img').setAttribute('src', imageURI);
+        }, function(err) {
+            console.log("Error calling the navigator.camera API");
+        }, {quality: 100});
+    };
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
